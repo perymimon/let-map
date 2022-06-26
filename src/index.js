5221374423089b7c3b7e066d3b3f1a5e820b80f5
perym <pery.mimon@gmail.com>
2022-06-27 00:45:09 +0300

    initial let-map

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export * from './let-map';
+// console.log(LetMap);
+// export default LetMap;
+
---
+++ src/let-map.js
@@ -0,0 +1,64 @@
+import EventEmitter from 'events';
+
+export const NEW = "NEW", UPDATE = "UPDATE", DELETE = "DELETE"
+
+export  class LetMap extends Map {
+    #emitter = new EventEmitter();
+
+    constructor(struct) {
+        super();
+        this.setStruct(struct);
+    }
+
+    on(...args) {
+        return this.#emitter.on(...args)
+    }
+
+    off(...args) {
+        return this.#emitter.off(...args)
+        return this.#emitter.off(...args)
+    }
+
+    // #emit(...args) {
+    //     this.#emitter.emit(...args);
+    //     this.#emitter.emit('update', ...args);
+    // }
+
+    /**
+     * @param struct can be object function or primitive
+     */
+    setStruct(struct) {
+        this.struct = struct;
+    }
+
+    set(k, nv) {
+        let ov = super.get(k);
+        super.set(k, nv);
+        const event = {key: k, phase: UPDATE, new: nv, old: ov}
+        this.#emitter.emit(k /*event name*/, event);
+        if (ov === void 0)
+            this.#emitter.emit('new', event); /*no event update for that*/
+        this.#emitter.emit('update', event);
+        return nv;
+    }
+
+    delete(k) {
+        let ov = super.get(k);
+        super.delete(k);
+        const event = {key: k, phase: DELETE, value: ov}
+        this.#emitter.emit(k /*event name*/, event);
+        this.#emitter.emit('delete', event);
+        this.#emitter.emit('update', event);
+    }
+
+    let(k, ...args) {
+        const {struct} = this;
+        if (struct && !super.has(k)) {
+            const s = typeof struct == 'function' ?
+                struct(k, ...args) : structuredClone(struct)
+            this.set(k, s)
+        }
+        return super.get(k);
+    }
+}
+
